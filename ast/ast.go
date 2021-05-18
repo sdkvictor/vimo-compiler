e73a91c61bff43781350ff6014dfa1ccd26ea4b0
Victor Villarreal <a01039863@itesm.mx>
2021-05-18 01:43:19 -0500

    progress ast

--- ast/ast.go
+++ ast/ast.go
@@ -1,7 +1,9 @@
 package ast
 
 import (
-    "foo/token"
+    "github.com/sdkvictor/golang-compiler/directories"
+	"github.com/sdkvictor/golang-compiler/gocc/token"
+	"github.com/sdkvictor/golang-compiler/types"
 )
 
 type Attrib interface {}
@@ -9,7 +11,7 @@ type Attrib interface {}
 type Program struct {
     functions 	[]*Function
 	id 			string
-	vars 		[]*dir.VarEntry
+	vars 		[]*directories.VarEntry
 }
 
 func (p *Program) Functions() []*Function {
@@ -23,7 +25,7 @@ func (p *Program) Id() string {
 type Function struct {
 	id        string
 	key       string
-	params    []*dir.VarEntry
+	params    []*directories.VarEntry
 	t         *types.Type
 	statement Statement
 	tok       *token.Token
@@ -104,5 +106,69 @@ func (s *Statement) isPredefinedFunction() bool {
 
 func (s *Statement) Token() *token.Token {
 	return i.tok
+}
+
+// Id is a wrapper for a string to represent an id for a variable as a statement
+type Id struct {
+	id  string
+	tok *token.Token
+}
+
+func (i *Id) Id() string {
+	return i.id
+}
+
+func (i *Id) Token() *token.Token {
+	return i.tok
+}
+
+type Assign struct {
+	id Id
+	attr Attribute
+	exp Expression
+	tok *token.Token
+}
+
+func (a *Assign) Id() string {
+	return a.id
+}
+
+func (a *Assign) Attribute() Attribute {
+	return a.attr
+}
+
+func (a *Assign) Expression() Expression {
+	return a.exp
+}
+
+func (a *Assign) Token() *token.Token {
+	return a.tok
+}
+
+type Condition struct {
+	exp Expression
+	stmts []Statement
+	elseStmts []Statement
+}
+
+// .... CONTINUE
+
+// ConstantValue defines a type with a single basic value
+type ConstantValue struct {
+	t     *types.Type
+	value string
+	tok   *token.Token
+}
+
+func (c *ConstantValue) Type() *types.Type {
+	return c.t
+}
+
+func (c *ConstantValue) Token() *token.Token {
+	return c.tok
+}
+
+func (c *ConstantValue) Value() string {
+	return c.value
 }
 
