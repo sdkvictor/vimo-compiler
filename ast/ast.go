92e6aafe96b3d4981bc3d4b9662b2e2ef7a9e153
Victor Villarreal <a01039863@itesm.mx>
2021-05-19 14:49:59 -0500

    ast pfuncs

--- ast/ast.go
+++ ast/ast.go
@@ -22,6 +22,13 @@ func (p *Program) Id() string {
 	return p.id
 }
 
+type Object struct {
+	id string
+	key string
+	t *types.Type
+	tok *token.Token
+}
+
 type Function struct {
 	id        string
 	key       string
@@ -30,6 +37,7 @@ type Function struct {
 	statement Statement
 	tok       *token.Token
 }
+
 
 func (f *Function) Id() string {
 	return f.id
@@ -175,7 +183,262 @@ type Write struct {
 }
 
 
+type Write struct {
+	exp Expression
+	str string
+	tok *token.Token
+}
+
+
 // .... CONTINUE
+
+type PredefinedFunction interface {
+	isSetColor()		bool
+	isSetSize() 		bool
+	isSetPosition() 	bool
+	isMove() 			bool
+	isSetText() 		bool
+	isKeyPressed() 		bool
+	isSetFontSize()		bool
+	isRender() 			bool
+	isLoadImage() 		bool
+	isCheckCollision() 	bool
+	isSetImage() 		bool
+	isPow() 			bool
+	isSquareRoot() 		bool
+	Token() *token.Token
+}
+
+func (s *Statement) isSetColor() bool {
+	return false
+}
+
+func (s *Statement) isMove() bool {
+	return false
+}
+func (s *Statement) isSetText() bool {
+	return false
+}
+func (s *Statement) isKeyPressed() bool {
+	return false
+}
+func (s *Statement) isSetFontSize() bool {
+	return false
+}
+func (s *Statement) isRender() bool {
+	return false
+}
+func (s *Statement) isLoadImage() bool {
+	return false
+}
+func (s *Statement) isCheckCollision() bool {
+	return false
+}
+func (s *Statement) isSetImage() bool {
+	return false
+}
+func (s *Statement) isPow() bool {
+	return false
+}
+func (s *Statement) isSquareRoot() bool {
+	return false
+}
+
+type SetColor struct {
+	color string
+	tok *token.Token
+}
+
+func (c *SetColor) Color() string {
+	return c.color
+}
+
+func (c *SetColor) Token() *token.Token {
+	return c.tok
+}
+
+type SetSize struct {
+	width int
+	height int
+	tok *token.Token
+}
+
+func (s *SetSize) Width() int {
+	return s.width
+}
+
+func (s *SetSize) Height() int {
+	return s.height
+}
+
+func (s *SetSize) Token() *token.Token {
+	return s.tok
+}
+
+type SetPosition struct {
+	x int
+	y int
+	tok *token.Token
+}
+
+func (p *SetPosition) X() int {
+	return p.x
+}
+
+func (p *SetPosition) Y() int {
+	return p.y
+}
+
+func (p *SetPosition) Token() *token.Token {
+	return p.tok
+}
+
+type Move struct {
+	x int
+	y int
+	tok *token.Token
+}
+
+func (m *Move) X() int {
+	return m.x
+}
+
+func (m *Move) Y() int {
+	return m.y
+}
+
+func (m *Move) Token() *token.Token {
+	return m.tok
+}
+
+type SetText struct {
+	txt string
+	tok *token.Token
+}
+
+func (t *SetText) Text() string {
+	return t.txt
+}
+
+func (t *SetText) Token() *token.Token {
+	return t.tok
+}
+
+type KeyPressed struct {
+	key string
+	tok *token.Token
+}
+
+func (k *KeyPressed) Key() string {
+	return k.key
+}
+
+func (t *SetText) Token() *token.Token {
+	return t.tok
+}
+
+type SetFontSize struct {
+	size int
+	tok *token.Token
+}
+
+func (f *SetFontSize) Size() int {
+	return f.size
+}
+
+func (f *SetFontSize) Token() *token.Token {
+	return f.tok
+}
+
+type Render struct {
+	obj Object
+	tok *token.Token
+}
+
+func (r *Render) Object() Object {
+	return r.obj
+}
+
+func (r *Render) Token() *token.Token {
+	return r.tok
+}
+
+
+
+type LoadImage struct {
+	image string
+	tok *token.Token
+}
+
+func (l *LoadImage) Image() string {
+	return l.image
+}
+
+func (l *LoadImage) Token() *token.Token {
+	return l.tok
+}
+
+type CheckCollision struct {
+	obj1 Object
+	obj2 Object
+	tok *token.Token
+}
+
+func (l *CheckCollision) Obj1() Object {
+	return l.obj1
+}
+
+func (l *CheckCollision) Obj2() Object {
+	return l.obj2
+}
+
+func (l *CheckCollision) Token() *token.Token {
+	return l.tok
+}
+
+type SetImage struct {
+	image string
+	tok *token.Token
+}
+
+func (i *SetImage) Image() string {
+	return i.image
+}
+
+func (i *SetImage) Token() *token.Token {
+	return i.tok
+}
+
+type Pow struct {
+	base 	float32
+	exp 	float32
+	tok *token.Token
+}
+
+func (p *Pow) Base() float32 {
+	return p.base
+}
+
+func (p *Pow) Exp() float32 {
+	return p.exp
+}
+
+func (p *Pow) Token() *token.Token {
+	return p.tok
+}
+
+type SquareRoot struct {
+	base float32
+	tok *token.Token
+}
+
+func (r *SquareRoot) Base() float32 {
+	return r.base
+}
+
+func (r *SquareRoot) Token() *token.Token {
+	return r.tok
+}
 
 // ConstantValue defines a type with a single basic value
 type ConstantValue struct {
